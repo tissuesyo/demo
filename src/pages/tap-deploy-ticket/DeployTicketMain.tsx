83a76ea0e0a793e6ce48f431bc111b2b20c908e8
Tissue <tissuesyo@gmail.com>
2024-03-07 23:53:18 +0800

    update edit and create mode

--- src/pages/tap-deploy-ticket/DeployTicketMain.tsx
+++ src/pages/tap-deploy-ticket/DeployTicketMain.tsx
@@ -1,27 +1,21 @@
 import { CopyOutlined, FileDoneOutlined, LinkOutlined, ReloadOutlined, RocketOutlined, UserAddOutlined } from '@ant-design/icons';
-import { Button, Collapse, CollapseProps, Input, Switch, theme } from 'antd';
+import { Button, Input, Switch, Tabs, theme } from 'antd';
 import React, { useState } from 'react';
-import ChangeList from './ChangeList';
 import ToolCriteria from './ToolCriteria';
 
 const DeployTicketMain: React.FC = () => {
-  const [isCreateMode, setDisplayMode] = useState('Create');
+  const [isCreateMode, setDisplayMode] = useState(true);
   const { token } = theme.useToken();
-  const collapseItems: CollapseProps['items'] = [
+  const tabs = [
     {
-      key: '1',
-      label: 'Tool Information',
+      label: 'Change Information',
+      key: 'changeList',
       children: <ToolCriteria />,
-    },
-    {
-      key: '2',
-      label: 'Change Summary',
-      children: <ChangeList />,
     }
-  ];
+  ]
 
   const changeDisplayMode = (isCreateMode: boolean) => {
-    setDisplayMode(isCreateMode ? 'Create' : 'Edit');
+    setDisplayMode(isCreateMode);
   };
 
   return (
@@ -30,21 +24,27 @@ const DeployTicketMain: React.FC = () => {
       <div className='mb-3'>
         <div><Input placeholder="Please input title" /></div>
       </div>
-      <div className="button-group">
-        <Button icon={<FileDoneOutlined />}>Submit</Button>
-        {/* <Button type="primary" icon={<FileDoneOutlined />}>Submit</Button> */}
-        {
-
-        }
-        <Button icon={<RocketOutlined />}>Deploy</Button>
-        <Button icon={<ReloadOutlined />}>Rollback</Button>        
-        <Button icon={<CopyOutlined />}>Clone</Button>
-        <Button icon={<UserAddOutlined />}>Assign</Button>
-        <Button icon={<LinkOutlined />}>Copy Case URL</Button>
-        <Switch checkedChildren="Create" unCheckedChildren="Edit" defaultChecked onChange={changeDisplayMode}/>
+      <div className="flex justify-between">
+        <div className="button-group">
+          <Button icon={<FileDoneOutlined />}>Submit</Button>
+          {/* <Button type="primary" icon={<FileDoneOutlined />}>Submit</Button> */}
+          {
+            !isCreateMode && <>
+            <Button icon={<RocketOutlined />}>Deploy</Button>
+            <Button icon={<ReloadOutlined />}>Rollback</Button>        
+            <Button icon={<CopyOutlined />}>Clone</Button>
+            <Button icon={<UserAddOutlined />}>Assign</Button>
+            <Button icon={<LinkOutlined />}>Copy Case URL</Button>
+            </>
+          }
+        </div>
+        <Switch checkedChildren="Create" unCheckedChildren="Edit" defaultChecked onChange={changeDisplayMode} className='float-right'/>
       </div>
       <div className="my-3">
-        <Collapse defaultActiveKey={['1', '2']} ghost items={collapseItems} />
+        {/* <Collapse defaultActiveKey={['1', '2']} ghost items={collapseItems} /> */}
+        {/* <ToolCriteria />
+        <ChangeList /> */}
+        <Tabs type="card" items={tabs}/>
       </div>
     </div>
     </>
